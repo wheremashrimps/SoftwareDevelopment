73ef15315c91d3fc7c22561acafb33b0045d7547
Matt Blair <blairmat@sheridanc.ca>
2022-04-19 14:00:12 -0700

    first commit

---
+++ src/War.java
@@ -0,0 +1,18 @@
+public class War extends Game {
+
+    public War(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        
+    }
+
+    @Override
+    public void declareWinner() {
+        
+    }
+    
+    
+}
